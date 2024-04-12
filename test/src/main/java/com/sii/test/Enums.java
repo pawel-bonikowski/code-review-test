9b594e0fe0923714a07b749f07af909bcbcaace9
pbonikowski <pawelbon@ext.inditex.com>
2024-04-12 13:52:59 +0200

    Enum - solution

--- test/src/main/java/com/sii/test/Enums.java
+++ test/src/main/java/com/sii/test/Enums.java
@@ -2,6 +2,9 @@ package com.sii.test;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Refactor the code.  Please use enum that will support Strategy Pattern
  */
@@ -14,51 +17,89 @@ public class Enums {
       String operation = args[1];
       final double a = Double.parseDouble(args[0]); 
       final double b = Double.parseDouble(args[2]);
-      
-      Plus plusOperation = new Plus();
-      Minus minusOperation = new Minus();
-      Multiply multiplyOperation = new Multiply();
-      Divide divideOperation = new Divide();
-      Power powerOperation = new Power();
-      
-      if (operation.equals("+")) {System.out.println(plusOperation.calculate(a,b));}
-      if (operation.equals("-")) {System.out.println(minusOperation.calculate(a,b));}
-      if (operation.equals("*")) {System.out.println(multiplyOperation.calculate(a,b));}
-      if (operation.equals("/")) {System.out.println(divideOperation.calculate(a,b));}
-      if (operation.equals("^")) {System.out.println(powerOperation.calculate(a,b));}
+
+      Operator operator = Operator.getBySymbol(operation);
+      double calculate = operator.calculate(a, b);
+      System.out.println(calculate);
+
+//      Plus plusOperation = new Plus();
+//      Minus minusOperation = new Minus();
+//      Multiply multiplyOperation = new Multiply();
+//      Divide divideOperation = new Divide();
+//      Power powerOperation = new Power();
+//      
+//      if (operation.equals("+")) {System.out.println(plusOperation.calculate(a,b));}
+//      if (operation.equals("-")) {System.out.println(minusOperation.calculate(a,b));}
+//      if (operation.equals("*")) {System.out.println(multiplyOperation.calculate(a,b));}
+//      if (operation.equals("/")) {System.out.println(divideOperation.calculate(a,b));}
+//      if (operation.equals("^")) {System.out.println(powerOperation.calculate(a,b));}
    }
 }
 
 
-class Plus  {
+enum Operator {
+   PLUS("+", new Plus()),
+   MINUS("-", new Minus()),
+   MULTI("*", new Multiply()),
+   DIVIDE("*", new Divide()),
+   POW("^", new Plus()),
+   ;
+
+   private final String operationSymbol;
+   
+   private final Calculator calculator;
+
+   Operator(String operationSymbol, Calculator calculator) {
+      this.operationSymbol = operationSymbol;
+       this.calculator = calculator;
+   }
+
+   public static Operator getBySymbol(String operationSymbol) {
+      return Arrays.stream(values())
+              .filter(i -> Objects.equals(i.operationSymbol, operationSymbol))
+              .findFirst()
+              .orElseThrow(UnsupportedOperationException::new);
+   }
+   
+   public double calculate(double a, double b) {
+      return calculator.calculate(a, b);
+   }
+
+}
+
+interface Calculator {
+   double calculate(double a, double b);
+}
+
+class Plus implements Calculator {
    
    public double calculate(double a, double b) {
       return a + b;
    }
 }
 
-class Minus {
+class Minus implements Calculator{
    
    public double calculate(double a, double b) {
       return a - b;
    }
 }
 
-class Multiply  {
+class Multiply implements Calculator  {
    
    public double calculate(double a, double b) {
       return a * b;
    }
 }
 
-class Divide  {
+class Divide implements Calculator {
    
    public double calculate(double a, double b) {
       return a / b;
    }
 }
 
-class Power  {
+class Power implements Calculator {
    
    public double calculate(double a, double b) {
       return Math.pow(a, b);
