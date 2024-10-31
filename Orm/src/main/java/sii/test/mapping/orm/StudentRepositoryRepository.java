2c89c57a19ff8721af5e54418d8c2944a9126e6c
pawel-bonikowski <bonikowski.pawel@gmail.com>
2024-10-31 10:34:10 +0100

    Orm tasks fixes

--- Orm/src/main/java/sii/test/mapping/orm/StudentRepositoryRepository.java
+++ Orm/src/main/java/sii/test/mapping/orm/StudentRepositoryRepository.java
@@ -7,6 +7,21 @@ import sii.test.mapping.orm.model.StudentAverageScoreView;
 
 import java.util.List;
 
+/*
+ * Expected table structure:
+ *   table students (
+ *        id bigint not null,
+ *        name varchar(255),
+ *        surname varchar(255)
+ *   );
+ *
+ *   table subjects (
+ *        id bigint not null,
+ *        final_score numeric(38,2),
+ *        name varchar(255),
+ *        student_id bigint
+ *   );
+ */
 public interface StudentRepositoryRepository extends JpaRepository<Student, Long> {
 
     @Query(nativeQuery = true, value = "@TODO write query")
